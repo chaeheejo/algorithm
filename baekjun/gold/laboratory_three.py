ae35e8dba12a6ad6f9b3a7383d72403033ba3c72
chaeheejo <60cogml@gmail.com>
2022-10-06 14:25:50 +0900

    solve laboratory_three 17142

--- baekjun/gold/laboratory_three.py
+++ baekjun/gold/laboratory_three.py
@@ -1,76 +1,65 @@
 N, M = map(int, input().split())
-virus_map = [list(input().split()) for _ in range(N)]
+virus_map = [list(map(int, input().split())) for _ in range(N)]
 
-virus_location = []
-blank_cnt=0
+virus=[]
+wall=[]
 for i in range(N):
     for j in range(N):
-        if virus_map[i][j]=='1':
-            virus_map[i][j]=-100
-        elif virus_map[i][j]=='2':
-            virus_location.append((i,j))
-            virus_map[i][j]=-200
-        elif virus_map[i][j]=='0':
-            virus_map[i][j]=-300
-            blank_cnt+=1
+        if virus_map[i][j]==1:
+            wall.append((i,j))
+        elif virus_map[i][j]==2:
+            virus.append((i, j))
 
-def combination(arr, num):
+def comb(arr, num):
     for i in range(len(arr)):
         if num==1:
             yield [arr[i]]
         else:
-            for next in combination(arr[i+1:], num-1):
+            for next in comb(arr[i+1:], num-1):
                 yield [arr[i]]+next
 
-chosen_location=[]
-for i in combination(range(len(virus_location)) ,M):
-    chosen_location.append(i)
+activate_virus_index=[]
+virus_num=len(virus)
+for i in comb(range(virus_num), M):
+    activate_virus_index.append(i)
 
-def max_cycle(map):
-    tmp=[]
+answer=9999
+xdirect = [-1,1,0,0]
+ydirect = [0,0,-1,1]
+for activate in activate_virus_index:
+    visited = [[-1] * N for _ in range(N)]
+    queue=[]
+    cnt=0
+
+    for a in activate:
+        i, j = virus[a]
+        visited[i][j]=0
+        queue.append((i,j))
+    for w in wall:
+        i, j = w
+        visited[i][j]=0
+
+    cur=0
+    while cur<len(queue):
+        i, j = queue[cur]
+        cur+=1
+
+        for k in range(4):
+            tmpI, tmpJ = i+xdirect[k], j+ydirect[k]
+            if 0<=tmpI<N and 0<=tmpJ<N and visited[tmpI][tmpJ]==-1 :
+                queue.append((tmpI, tmpJ))
+                visited[tmpI][tmpJ]=visited[i][j]+1
+                if virus_map[tmpI][tmpJ]==0:
+                    cnt = visited[tmpI][tmpJ]
+    solved=True
     for i in range(N):
-        tmp.append(max(map[i]))
-    return max(tmp)
+        if visited[i].count(-1):
+            solved=False
+            break
+    if solved:
+        answer = min(answer, cnt)
 
-answer=[]
-for i in range(len(chosen_location)):
-    tmp_map=virus_map[:]
-    global cnt
-    cnt=blank_cnt
-
-    def virus_spread(spread_index):
-        global cnt
-        if cnt==0:
-            return
-        i, j = spread_index
-        if -1<i-1<N and -1<j<N:
-            if tmp_map[i-1][j]==-300:
-                tmp_map[i-1][j]=tmp_map[i][j] + 1
-                cnt-=1
-                virus_spread([i-1,j])
-        if -1<i+1<N and -1<j<N:
-            if tmp_map[i+1][j]==-300:
-                tmp_map[i+1][j]=tmp_map[i][j] + 1
-                cnt -= 1
-                virus_spread([i+1, j])
-        if -1<i<N and -1<j+1<N:
-            if tmp_map[i][j+1] == -300:
-                tmp_map[i][j+1] = tmp_map[i][j] + 1
-                cnt -= 1
-                virus_spread([i, j+1])
-        if -1<i<N and -1<j-1<N:
-            if tmp_map[i][j-1] == -300:
-                tmp_map[i][j-1] = tmp_map[i][j] + 1
-                cnt -= 1
-                virus_spread([i, j-1])
-
-
-    for index in range(len(virus_location)):
-        if index in chosen_location[i]:
-            i, j = virus_location[index]
-            tmp_map[i][j] = 0
-            virus_spread([i, j])
-
-    answer.append(max_cycle(tmp_map))
-
-print(answer)
+if answer==9999:
+    print(-1)
+else:
+    print(answer)
