1095bec292eb1244d54b14ef46fc9f1b96d5bc2f
chaeheejo <60cogml@gmail.com>
2022-10-12 18:41:09 +0900

    solve shark_elementary_school 21608

--- baekjun/gold/shark_elementary_school.py
+++ baekjun/gold/shark_elementary_school.py
@@ -1,86 +1,42 @@
 N = int(input())
-like = [list(map(int, input().split())) for _ in range(N*N)]
+student_like = [list(map(int, input().split())) for _ in range(N*N)]
 seat = [[0]*N for _ in range(N)]
 
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
-def check_adjacent(condition):
-    maximum = 0
+for n in range(N*N):
+    student = student_like[n]
+    tmp=[]
+
     for i in range(N):
         for j in range(N):
-            for k in range(4):
-                tmpI, tmpJ = i + dx[k], j + dy[k]
-                if 0 > tmpI or tmpI >= N or 0 > tmpJ or tmpJ >= N:
-                    continue
-                if seat[tmpI][tmpJ] in condition:
-                    probability[i][j] += 1
+            if seat[i][j]==0:
+                like=0
+                blank=0
+                for k in range(4):
+                    tx, ty = i+dx[k], j+dy[k]
+                    if 0<=tx<N and 0<=ty<N:
+                        if seat[tx][ty] in student[1:]:
+                            like+=1
+                        if seat[tx][ty]==0:
+                            blank+=1
+                tmp.append([like, blank, i, j])
+    tmp.sort(key= lambda f:(-f[0], -f[1], f[2], f[3]))
+    seat[tmp[0][2]][tmp[0][3]] = student[0]
 
-                    if probability[i][j] > maximum:
-                        maximum = probability[i][j]
-    return maximum
+answer=0
+student_like.sort()
 
-for k in range(N):
-    n, a, b, c, d = like[k]
-    probability=[[0]*N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        total=0
+        for k in range(4):
+            tx, ty = i+dx[k], j+dy[k]
+            if 0<=tx<N and 0<=ty<N:
+                if seat[tx][ty] in student_like[seat[i][j]-1]:
+                    total+=1
+        if total!=0:
+            answer+=10**(total-1)
 
-    maximum=check_adjacent([a,b,c,d])
-
-    candidate=[]
-    for i in range(N):
-        for j in range(N):
-            if maximum==probability[i][j]:
-                candidate.append((i,j))
-
-    if len(candidate)==1:
-        i, j = candidate[0]
-        seat[i][j]=n
-        continue
-
-    probability = [[0] * N for _ in range(N)]
-    maximum = check_adjacent([0])
-
-    candidate = []
-    for i in range(N):
-        for j in range(N):
-            if maximum == probability[i][j]:
-                candidate.append((i, j))
-
-    if len(candidate)==1:
-        i, j = candidate[0]
-        seat[i][j]=n
-        continue
-
-    minimumI=999
-    for i,j in candidate:
-        if minimumI>i:
-            minimumI=i
-
-    cnt=0
-    cndI, cndJ = 0,0
-    for i, j in candidate:
-        if minimumI==i:
-            cnt+=1
-            cndI, cndJ = i, j
-
-    if cnt==1:
-        seat[cndI][cndJ]=n
-
-    minimumJ = 999
-    for i, j in candidate:
-        if minimumJ>j:
-            minimumJ=j
-
-    cndI, cndJ = 0, 0
-    for i, j in candidate:
-        if minimumJ == j:
-            cndI, cndJ = i, j
-
-    seat[cndI][cndJ]=n
-
-for j in range(N):
-    for k in range(4):
-        tmpI, tmpJ = dx[k], j + dy[k]
-        if 0 > tmpI or tmpI >= N or 0 > tmpJ or tmpJ >= N:
-            continue
-        if seat[tmpI][tmpJ] in like[]
+print(answer)
